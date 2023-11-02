cc41646d97c0aa249a1d374cbd2fabc9c7db45dc
allone9425 <allone9425@gmail.com>
2023-11-02 11:37:07 +0900

    내일배움캠프 리액트 입문

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+const App = () => {
+  const testArr = ["감자", "고구마", "오이", "가지", "옥수수"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-style">
+      {testArr
+        .filter(function (item) {
+          return item !== "오이";
+        })
+        .map(function (item) {
+          return <div className="component-style">{item}</div>;
+        })}
     </div>
   );
-}
+};
 
 export default App;
